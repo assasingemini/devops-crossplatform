fd2d020967bc597455a4247d62e2d6e459cbcc65
Besna <152285973+assasingemini@users.noreply.github.com>
2025-08-11 17:25:43 +0700

    Add Windows container + CodeBuild buildspecs; rename Dockerfile to Dockerfile.linux

---
+++ main.ps1
@@ -0,0 +1,15 @@
+﻿ = "http://*:8080/"
+ = New-Object System.Net.HttpListener
+.Prefixes.Add()
+.Start()
+while (True) {
+   = .GetContext()
+   = .Response
+   = "Hello from Windows container!
+"
+   = [System.Text.Encoding]::UTF8.GetBytes()
+  .StatusCode = 200
+  .ContentType = "text/plain"
+  .OutputStream.Write(, 0, .Length)
+  .Close()
+}
